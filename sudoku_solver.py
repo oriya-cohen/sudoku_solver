b59dc24b44e98343ace8c8e732ff243327b57c5f
oriya <oriyachn@gmail.com>
2021-01-17 00:42:59 +0200

    sud_01 current workflow: image -> gray -> bilateral filter

--- sudoku_solver.py
+++ sudoku_solver.py
@@ -1,149 +1,173 @@
 import numpy as np
 import cv2
 
+#
+# class line_obj:
+#     def __init__(self, *argv):
+#         self.x1, self.y1, self.x2, self.y2 = argv[0]
+#         self.dif_x = float(self.x2 - self.x1)
+#         self.dif_y = float(self.y2 - self.y1)
+#         self.length_line = np.sqrt(np.power(self.dif_x, 2) + np.power(self.dif_y, 2))
+#         self.theta = np.arctan(np.divide(self.dif_y, self.dif_x))
+#
+#     def line_extend(self):
+#         new_x1 = self.x1 - int(10 * self.dif_x / self.length_line)
+#         new_x2 = self.x2 + int(10 * self.dif_x / self.length_line)
+#         new_y1 = self.y1 - int(10 * self.dif_y / self.length_line)
+#         new_y2 = self.y2 + int(10 * self.dif_y / self.length_line)
+#
+#         line_extend = [[new_x1, new_y1, new_x2, new_y2]]
+#         return line_extend
+#
+#     def line_theta(self):
+#         return self.theta
+#
+#
+# def get_height():
+#     height = img_dimension[0]
+#     return height
 
-class line_obj:
-    def __init__(self, *argv):
-        self.x1, self.y1, self.x2, self.y2 = argv[0]
-        self.dif_x = float(self.x2 - self.x1)
-        self.dif_y = float(self.y2 - self.y1)
-        self.length_line = np.sqrt(np.power(self.dif_x, 2) + np.power(self.dif_y, 2))
-        self.theta = np.arctan(np.divide(self.dif_y, self.dif_x))
 
-    def line_extend(self):
-        new_x1 = self.x1 - int(10 * self.dif_x / self.length_line)
-        new_x2 = self.x2 + int(10 * self.dif_x / self.length_line)
-        new_y1 = self.y1 - int(10 * self.dif_y / self.length_line)
-        new_y2 = self.y2 + int(10 * self.dif_y / self.length_line)
+def edit_frame(frame_to_edit):
 
-        line_extend = [[new_x1, new_y1, new_x2, new_y2]]
-        return line_extend
-
-    def line_theta(self):
-        return self.theta
+    edited_frame = cv2.cvtColor(frame_to_edit, cv2.COLOR_BGR2GRAY)
+    return edited_frame
 
 
-def get_height():
-    height = img_dimension[0]
-    return height
+if __name__ == "__main__":
+    # get figure from video
+    print('Hello')
+    cap = cv2.VideoCapture(0)
+    cap.release()
+    ret, frame = cap.read()
+
+    while ret and cv2.waitKey(1) & 0xFF == 'q':
+        cv2.inshow(frame, "Normal")           # display video
+        edited_frame = edit_frame(frame)
+        cv2.inshow(edited_frame, "Edited")    # display edited video
+        ret, frame = cap.read()               # read next frame
 
 
-# get figure
-print('starting ')
 
-# load fig grey scale
-original_img = cv2.imread(r'D:\\PyProjects\\sudoku\\pics\\her1.jpeg', 0)
-(thresh, BW_image) = cv2.threshold(original_img, 127, 255, cv2.THRESH_BINARY)
+    cap.release()
+    cv2.destroyAllWindows()
 
-# d - size
-img_dimension = BW_image.shape
-
-height = get_height()
-
-ratio = 450 / height
-d_size = [ratio * img_dimension[0], ratio * img_dimension[1]]
-d_size = [int(i) for i in d_size[::-1]]
-
-# resize image
-resized_img = cv2.resize(BW_image, tuple(d_size))
-cv2.imshow('1. resized_img', resized_img)
+# # get figure
+# print('starting ')
+#
+# # load fig grey scale
+# original_img = cv2.imread(r'D:\\PyProjects\\sudoku\\pics\\her1.jpeg', 0)
+# (thresh, BW_image) = cv2.threshold(original_img, 127, 255, cv2.THRESH_BINARY)
+#
+# # d - size
+# img_dimension = BW_image.shape
+#
+# height = get_height()
+#
+# ratio = 450 / height
+# d_size = [ratio * img_dimension[0], ratio * img_dimension[1]]
+# d_size = [int(i) for i in d_size[::-1]]
+#
+# # resize image
+# resized_img = cv2.resize(BW_image, tuple(d_size))
+# cv2.imshow('1. resized_img', resized_img)
+# # cv2.waitKey(0) & 0xFF
+#
+#
+# # Taking a matrix of size 3 as the kernel
+# kernel = np.ones((3, 3), np.uint8)
+# # The first parameter is the original image,
+# # kernel is the matrix with which image is
+# # convolved and third parameter is the number
+# # of iterations, which will determine how much
+# # you want to erode/dilate a given image.
+# img_dilation = cv2.dilate(resized_img, kernel, iterations=1)
+# img_erosion = cv2.erode(img_dilation, kernel, iterations=1)
+#
+# # cv2.imshow('Input', img)
+# cv2.imshow('2. Dilation', img_dilation)
+# cv2.imshow('3. Erosion', img_erosion)
+# # cv2.waitKey(0) & 0xFF
+#
+# # blur image
+#
+# # kernel_size = 5
+# # blur_gray = cv2.GaussianBlur(resized_img,(kernel_size, kernel_size),0)
+# # blur_gray = resized_img
+# blur_gray = img_erosion
+#
+# # show blurred scaled grayscale img
+# cv2.imshow('3. blured', blur_gray)
 # cv2.waitKey(0) & 0xFF
-
-
-# Taking a matrix of size 3 as the kernel 
-kernel = np.ones((3, 3), np.uint8)
-# The first parameter is the original image, 
-# kernel is the matrix with which image is  
-# convolved and third parameter is the number  
-# of iterations, which will determine how much  
-# you want to erode/dilate a given image.  
-img_dilation = cv2.dilate(resized_img, kernel, iterations=1)
-img_erosion = cv2.erode(img_dilation, kernel, iterations=1)
-
-# cv2.imshow('Input', img) 
-cv2.imshow('2. Dilation', img_dilation)
-cv2.imshow('3. Erosion', img_erosion)
+#
+# # identify area of sudoku
+#
+# # apply canny algorithm to identify edges
+# low_threshold = 50
+# high_threshold = 100
+# apertureSize = 3
+# edges = cv2.Canny(blur_gray, low_threshold, high_threshold, apertureSize)
+#
+# # show the difference img
+# cv2.imshow('edges', edges)
 # cv2.waitKey(0) & 0xFF
-
-# blur image 
-
-# kernel_size = 5
-# blur_gray = cv2.GaussianBlur(resized_img,(kernel_size, kernel_size),0)
-# blur_gray = resized_img
-blur_gray = img_erosion
-
-# show blurred scaled grayscale img
-cv2.imshow('3. blured', blur_gray)
-cv2.waitKey(0) & 0xFF
-
-# identify area of sudoku
-
-# apply canny algorithm to identify edges
-low_threshold = 50
-high_threshold = 100
-apertureSize = 3
-edges = cv2.Canny(blur_gray, low_threshold, high_threshold, apertureSize)
-
-# show the difference img
-cv2.imshow('edges', edges)
-cv2.waitKey(0) & 0xFF
-
-# Run Hough on edge detected image
-# parameters
-rho = 1  # distance resolution in pixels of the Hough grid
-theta = np.pi / 180  # angular resolution in radians of the Hough grid
-threshold = 100  # minimum number of votes (intersections in Hough grid cell)
-min_line_length = 50  # minimum number of pixels making up a line
-max_line_gap = 20  # maximum gap in pixels between connectable line segments
-
-# Output "lines" is an array containing endpoints of detected line segments
-lines = cv2.HoughLinesP(edges, rho, theta, threshold, np.array([]), min_line_length, max_line_gap)
-# lines = [ line_extend(line) for line in lines]
-
-# group lines by angle
-# lines.sort( key = lambda x: line_obj(x[0]).line_theta()) #__________________ correct it later
-line_obj_vec = [line_obj(k[0]) for k in lines]
-
-# New image to put lines on
-line_image = np.copy(blur_gray) * 0  # creating a blank to draw lines on
-
-for line in lines:
-
-    for x1, y1, x2, y2 in line:
-        cv2.line(line_image, (x1, y1), (x2, y2), (255, 0, 0), 2)
-
-'''
-for idx,dat_line in enumerate(lines):   # -> lines[0]
-    
-    rho,theta=dat_line[0]
-    print(['line ',idx])
-    a = np.cos(theta)
-    b = np.sin(theta)
-    x0 = a*rho
-    y0 = b*rho
-    x1 = int(x0 + 1000*(-b))
-    y1 = int(y0 + 1000*(a))
-    x2 = int(x0 - 1000*(-b))
-    y2 = int(y0 - 1000*(a))
-    cv2.line(img,(x1,y1),(x2,y2),(0,0,255),2)
-'''
-
-cv2.imshow('Hough lines results', line_image)
-cv2.waitKey(0) & 0xFF
-
-# straiten the figure
-
-
-# getting all 81 squere subfigures and put to a figure list
-
-
-# for each sub figure run mnist-trained network and get the number to an array
-
-
-# run an algorithem to solve sudoku
-
-
-# display solution to screan.
-
-print('The End')
-# input("Press Enter to continue...")
+#
+# # Run Hough on edge detected image
+# # parameters
+# rho = 1  # distance resolution in pixels of the Hough grid
+# theta = np.pi / 180  # angular resolution in radians of the Hough grid
+# threshold = 100  # minimum number of votes (intersections in Hough grid cell)
+# min_line_length = 50  # minimum number of pixels making up a line
+# max_line_gap = 20  # maximum gap in pixels between connectable line segments
+#
+# # Output "lines" is an array containing endpoints of detected line segments
+# lines = cv2.HoughLinesP(edges, rho, theta, threshold, np.array([]), min_line_length, max_line_gap)
+# # lines = [ line_extend(line) for line in lines]
+#
+# # group lines by angle
+# # lines.sort( key = lambda x: line_obj(x[0]).line_theta()) #__________________ correct it later
+# line_obj_vec = [line_obj(k[0]) for k in lines]
+#
+# # New image to put lines on
+# line_image = np.copy(blur_gray) * 0  # creating a blank to draw lines on
+#
+# for line in lines:
+#
+#     for x1, y1, x2, y2 in line:
+#         cv2.line(line_image, (x1, y1), (x2, y2), (255, 0, 0), 2)
+#
+# '''
+# for idx,dat_line in enumerate(lines):   # -> lines[0]
+#
+#     rho,theta=dat_line[0]
+#     print(['line ',idx])
+#     a = np.cos(theta)
+#     b = np.sin(theta)
+#     x0 = a*rho
+#     y0 = b*rho
+#     x1 = int(x0 + 1000*(-b))
+#     y1 = int(y0 + 1000*(a))
+#     x2 = int(x0 - 1000*(-b))
+#     y2 = int(y0 - 1000*(a))
+#     cv2.line(img,(x1,y1),(x2,y2),(0,0,255),2)
+# '''
+#
+# cv2.imshow('Hough lines results', line_image)
+# cv2.waitKey(0) & 0xFF
+#
+# # straiten the figure
+#
+#
+# # getting all 81 squere subfigures and put to a figure list
+#
+#
+# # for each sub figure run mnist-trained network and get the number to an array
+#
+#
+# # run an algorithem to solve sudoku
+#
+#
+# # display solution to screan.
+#
+# print('The End')
+# # input("Press Enter to continue...")
---
+++ sudoku_solver01.py
@@ -0,0 +1,146 @@
+import cv2
+import numpy as np
+import math
+
+
+def image_resize(image, width=None, height=None, inter=cv2.INTER_AREA):
+    # initialize the dimensions of the image to be resized and
+    # grab the image size
+    dim = None
+    (h, w) = image.shape[:2]
+
+    # if both the width and height are None, then return the
+    # original image
+    if width is None and height is None:
+        return image
+
+    # check to see if the width is None
+    if width is None:
+        # calculate the ratio of the height and construct the
+        # dimensions
+        r = height / float(h)
+        dim = (int(w * r), height)
+
+    # otherwise, the height is None
+    else:
+        # calculate the ratio of the width and construct the
+        # dimensions
+        r = width / float(w)
+        dim = (width, int(h * r))
+
+    # resize the image
+    resized = cv2.resize(image, dim, interpolation=inter)
+
+    # return the resized image
+    return resized
+
+
+def clear_image_spots(frame_to_clear):
+    # Creating kernel
+
+    # first try
+    # kernel = np.ones((3, 3), np.uint8)
+    # Using cv2.erode() method
+    # eroded_img = cv2.erode(frame_to_clear, kernel, cv2.BORDER_REFLECT)
+    # Using cv2.dilate() method
+    # Dilated_img = cv2.dilate(eroded_img, kernel, cv2.BORDER_REFLECT)
+
+    # second try
+    ## opening (erosion followed by dilation) and closing(the opposite)
+    # kernel = np.ones((3, 3), np.uint8)
+    # opening = cv2.morphologyEx(frame_to_clear, cv2.MORPH_OPEN, kernel)
+    # closing = cv2.morphologyEx(opening, cv2.MORPH_CLOSE, kernel)
+    # cleared_img = closing
+
+    # third try
+    cleared_img = cv2.bilateralFilter(frame_to_clear, 10, 20, 20)  # apply bilateral filter
+
+    # forth try
+    # kernel = np.ones((2, 2), np.uint8)
+    # cleared_img = cv2.adaptiveThreshold(frame_to_clear, 255, cv2.ADAPTIVE_THRESH_GAUSSIAN_C, cv2.THRESH_BINARY, 11, 2)
+    # cleared_img = cv2.morphologyEx(cleared_img, cv2.MORPH_CLOSE, kernel)
+
+    return cleared_img
+
+
+def boxed_frame(big_img, size_sub_window_0to1):
+    # taken from
+    # https://stackoverflow.com/questions/44650888/resize-an-image-without-distortion-opencv
+    # answer by https://stackoverflow.com/users/4907913/thewaywewere
+
+    height, width, channels = big_img.shape
+    min_dim = min(height, width)  # the minimal dimension
+    half_window_size = math.floor(min_dim * size_sub_window_0to1 / 2)
+    mid_frame = (math.ceil(height / 2), math.ceil(width / 2))
+
+    # creating list of (x,y) tuples for the edges of Range Of Interest (ROI)
+    y_idx = np.add(mid_frame[0], [-half_window_size, -half_window_size, +half_window_size, +half_window_size])
+    x_idx = np.add(mid_frame[1], [-half_window_size, +half_window_size, +half_window_size, -half_window_size])
+    points = [pt for pt in zip(x_idx, y_idx)]
+
+    # plot ROI on image
+    line_width = 1
+    sq_marked_img = big_img
+    sq_marked_img = cv2.rectangle(sq_marked_img, points[0], points[2], (100, 100, 100), line_width)
+
+    # drew that way but there is a shelf code for it
+    # for k in range(len(points)):
+    #     sq_marked_img = cv2.line(sq_marked_img, points[k - 1], points[k], (255, 0, 0), line_width)
+
+    sub_img = big_img[points[0][1]:points[2][1],
+              points[0][0]:points[2][0]]  # sub_img = img[y(pt1):y(pt3), x(pt1):x(pt3)]
+
+    sub_img = image_resize(sub_img, height=500)
+
+    return sq_marked_img, sub_img
+
+
+def edit_frame(frame_to_edit):
+
+    gray = cv2.cvtColor(frame_to_edit, cv2.COLOR_BGR2GRAY)  # gray color
+
+    # (thresh, frame_to_edit) = cv2.threshold(frame_to_edit, 127, 255, cv2.THRESH_BINARY)   # black and white img
+
+    spots_cleared_frame = clear_image_spots(gray)  # clear image spots
+    cv2.imshow('cleared_frame', spots_cleared_frame)
+    # find edges
+    frame_egdes = cv2.Canny(spots_cleared_frame, 50, 80)
+    cv2.imshow('frame_egdes', frame_egdes)
+
+    # find contours
+    ret, thresh = cv2.threshold(frame_egdes, 127, 255, 0)
+    contours, hierarchy = cv2.findContours(thresh, cv2.RETR_TREE, cv2.CHAIN_APPROX_SIMPLE)
+    frame_egdes = cv2.drawContours(frame_egdes, contours, -1, (255, 255, 255), 5)
+
+    # find lines
+    minLineLength = 100
+    maxLineGap = 100
+    lines = cv2.HoughLinesP(frame_egdes, 1, np.pi / 180, 20, minLineLength, maxLineGap)
+
+    try:
+        for line in lines:
+            x1, y1, x2, y2 = line[0]
+            frame_to_edit = cv2.line(frame_to_edit, (x1, y1), (x2, y2), (255, 0, 0), 2)
+    except:
+        print('nan found')
+
+    return frame_to_edit
+
+
+if __name__ == '__main__':
+    cap = cv2.VideoCapture(0, cv2.CAP_DSHOW)
+
+    while True:
+        ret, frame = cap.read()
+        if not ret or cv2.waitKey(1) & 0xFF == ord('q'):
+            break
+        range_sub_rect = 2 / 3
+        frame_box, sub_frame = boxed_frame(frame, range_sub_rect)
+        cv2.imshow('feed', frame_box)
+
+        edited_frame = edit_frame(sub_frame)
+        cv2.imshow('edited_frame', edited_frame)
+
+
+    cap.release()  # release the camera
+    cv2.destroyAllWindows()  # closes the current windows
