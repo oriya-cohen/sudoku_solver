f8441e2a8ec15538552d65e1de398c49564d2685
oriya <oriyachn@gmail.com>
2021-01-18 08:39:18 +0200

    sud_01 current workflow: image -> gray -> bilateral filter -> canny edges -> find contours on it -(-)> cancel HoughLinesP +>  take the biggest contour and find the sub figure containing sudoku -> warp image -> get numbers

--- sudoku_solver01.py
+++ sudoku_solver01.py
@@ -3,6 +3,88 @@ import numpy as np
 import math
 
 
+def get_numbers_in_fig(new_img, size=9):
+    x_len, y_len = new_img.shape
+
+    # size of mini image
+    x_mini = x_len // size
+    y_mini = y_len // size
+
+    sud_array = np.empty((size, size))
+
+    # create array of figures
+    for c in range(size):
+        for r in range(size):
+            sud_array = new_img[x_mini * r: x_mini * (r + 1), y_mini * c: y_mini * (c + 1)]
+
+            # test
+            if r == 0 and c == 2:
+                cv2.imshow('is it 8?', sud_array)
+
+    return sud_array
+
+
+def order_points(pts):
+    # https://www.pyimagesearch.com/2014/08/25/4-point-opencv-getperspective-transform-example/
+
+    # initialzie a list of coordinates that will be ordered
+    # such that the first entry in the list is the top-left,
+    # the second entry is the top-right, the third is the
+    # bottom-right, and the fourth is the bottom-left
+    rect = np.zeros((4, 2), dtype="float32")
+    # the top-left point will have the smallest sum, whereas
+    # the bottom-right point will have the largest sum
+    s = np.array(pts).sum(axis=1)
+    rect[0] = pts[np.argmin(s)]
+    rect[2] = pts[np.argmax(s)]
+    # now, compute the difference between the points, the
+    # top-right point will have the smallest difference,
+    # whereas the bottom-left will have the largest difference
+    diff = np.diff(pts, axis=1)
+    rect[1] = pts[np.argmin(diff)]
+    rect[3] = pts[np.argmax(diff)]
+    # return the ordered coordinates
+    return rect
+
+
+def four_point_transform(image, pts):
+    # https://www.pyimagesearch.com/2014/08/25/4-point-opencv-getperspective-transform-example/
+
+    # obtain a consistent order of the points and unpack them
+    # individually
+    rect = order_points(pts)
+    (tl, tr, br, bl) = rect
+    # compute the width of the new image, which will be the
+    # maximum distance between bottom-right and bottom-left
+    # x-coordiates or the top-right and top-left x-coordinates
+    widthA = np.sqrt(((br[0] - bl[0]) ** 2) + ((br[1] - bl[1]) ** 2))
+    widthB = np.sqrt(((tr[0] - tl[0]) ** 2) + ((tr[1] - tl[1]) ** 2))
+    maxWidth = max(int(widthA), int(widthB))
+    # compute the height of the new image, which will be the
+    # maximum distance between the top-right and bottom-right
+    # y-coordinates or the top-left and bottom-left y-coordinates
+    heightA = np.sqrt(((tr[0] - br[0]) ** 2) + ((tr[1] - br[1]) ** 2))
+    heightB = np.sqrt(((tl[0] - bl[0]) ** 2) + ((tl[1] - bl[1]) ** 2))
+    maxHeight = max(int(heightA), int(heightB))
+    # now that we have the dimensions of the new image, construct
+    # the set of destination points to obtain a "birds eye view",
+    # (i.e. top-down view) of the image, again specifying points
+    # in the top-left, top-right, bottom-right, and bottom-left
+    # order
+    dst = np.array([
+        [0, 0],
+        [maxWidth - 1, 0],
+        [maxWidth - 1, maxHeight - 1],
+        [0, maxHeight - 1]], dtype="float32")
+    # compute the perspective transform matrix and then apply it
+    M = cv2.getPerspectiveTransform(rect, dst)
+    warped = cv2.warpPerspective(image, M, (maxWidth, maxHeight))
+    # return the warped image
+    # and the transverse coordinate transform
+    transverse = np.linalg.inv(M)
+    return warped, transverse
+
+
 def image_resize(image, width=None, height=None, inter=cv2.INTER_AREA):
     # initialize the dimensions of the image to be resized and
     # grab the image size
@@ -100,15 +182,15 @@ def line_dist(line1, line2):
 
 
 def line_theta(line):
-    return math.atan(   (line[0][3] - line[0][1]) / (line[0][2] - line[0][0])   )
+    return math.atan((line[0][3] - line[0][1]) / (line[0][2] - line[0][0]))
 
 
 def unite_lines(lines):
     try:
         # sort by cos(theta) of the line
         united_lns = sorted(lines,
-                            key=lambda line: math.cos(line_theta(line)) )
-        grouped_lines = united_lns                                            ### edit this ###
+                            key=lambda line: math.cos(line_theta(line)))
+        grouped_lines = united_lns  ### edit this ###
         for line in lines:
             x1, y1, x2, y2 = line[0]
             frame_to_edit = cv2.line(frame_to_edit, (x1, y1), (x2, y2), (255, 0, 0), 2)
@@ -119,7 +201,6 @@ def unite_lines(lines):
 
 
 def edit_frame(frame_to_edit):
-
     gray = cv2.cvtColor(frame_to_edit, cv2.COLOR_BGR2GRAY)  # gray color
 
     # (thresh, frame_to_edit) = cv2.threshold(frame_to_edit, 127, 255, cv2.THRESH_BINARY)   # black and white img
@@ -132,24 +213,55 @@ def edit_frame(frame_to_edit):
 
     # find contours
     ret, thresh = cv2.threshold(frame_egdes, 127, 255, 0)
-    contours, hierarchy = cv2.findContours(thresh, cv2.RETR_TREE, cv2.CHAIN_APPROX_SIMPLE)
+    # contours, hierarchy = cv2.findContours(thresh, cv2.RETR_TREE, cv2.CHAIN_APPROX_SIMPLE)
+    contours, hierarchy = cv2.findContours(thresh, cv2.cv2.RETR_EXTERNAL, cv2.CHAIN_APPROX_SIMPLE) # only the external contour
     frame_egdes = cv2.drawContours(frame_egdes, contours, -1, (255, 255, 255), 6)
     cv2.imshow('frame_egdes', frame_egdes)
 
-    # find lines
-    minLineLength = 100
-    maxLineGap = 100
-    # lines = cv2.HoughLinesP(frame_egdes, 1, np.pi / 180, 20, minLineLength, maxLineGap)
-    lines = cv2.HoughLinesP(frame_egdes, 1, 5 * np.pi / 180, 20, minLineLength, maxLineGap)
+    # sort contours by area - this part is based on :
+    #           https://github.com/AjayAndData/Licence-plate-detection-and-recognition---using-openCV-only
+    #           /blob/master/Car%20Number%20Plate%20Detection.py
+    contours = sorted(contours, key=cv2.contourArea, reverse=True)[:max(5, len(contours))]
 
-    # unite lines
-    # lines = unite_lines(lines)
-    try:
-        for line in lines:
-            x1, y1, x2, y2 = line[0]
-            frame_to_edit = cv2.line(frame_to_edit, (x1, y1), (x2, y2), (255, 0, 0), 2)
-    except:
-        print('nan found')
+    # we need the maximal squared area
+    idx = 1
+    for cntr in contours:
+        perimeter = cv2.arcLength(cntr, True)
+        approx = cv2.approxPolyDP(cntr, 0.02 * perimeter, True)
+        # print ("approx = ",approx)
+        if len(approx) == 4:  # Select the contour with 4 corners
+            NumberPlateCnt = approx  # This is our approx Number Plate Contour
+
+            # Crop those contours and store it in Cropped Images folder
+            # x, y, w, h = cv2.boundingRect(cntr)  # This will find out co-ord for plate
+            # new_img = gray[y:y + h, x:x + w]  # Create new image
+
+            # my edition. stretching a Quadrilateral to square
+            new_img = four_point_transform(gray, pts=[approx[i][0] for i in range(4)])
+
+            if np.size(new_img) > 0.5 * np.size(gray):   # only for large figures display them
+                cv2.imshow('Cropped Images /' + str(idx) + '.png', new_img)  # display new image
+                # cv2.imwrite('Cropped Images-Text/' + str(idx) + '.png', new_img)  # Store new image
+                # idx += 1
+
+                # pass the image to image recognition solver
+                soduko_arr = get_numbers_in_fig(new_img)
+            break
+
+    # find lines
+    # minLineLength = 100
+    # maxLineGap = 100
+    # # lines = cv2.HoughLinesP(frame_egdes, 1, np.pi / 180, 20, minLineLength, maxLineGap)
+    # lines = cv2.HoughLinesP(frame_egdes, 1, 5 * np.pi / 180, 20, minLineLength, maxLineGap)
+    #
+    # # unite lines
+    # # lines = unite_lines(lines)
+    # try:
+    #     for line in lines:
+    #         x1, y1, x2, y2 = line[0]
+    #         frame_to_edit = cv2.line(frame_to_edit, (x1, y1), (x2, y2), (255, 0, 0), 2)
+    # except:
+    #     print('nan found')
 
     return frame_to_edit
 
@@ -167,7 +279,6 @@ if __name__ == '__main__':
 
         edited_frame = edit_frame(sub_frame)
         cv2.imshow('edited_frame', edited_frame)
-
 
     cap.release()  # release the camera
     cv2.destroyAllWindows()  # closes the current windows
